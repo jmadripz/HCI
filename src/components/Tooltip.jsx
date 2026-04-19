7d5a7c761442d2c7e0f6847e0e25dbccf0613205
Lwachterr <lwacht3@lsu.edu>
2026-04-19 18:32:21 -0500

    Move local tooltip changes to tooltip_finish

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,23 +1,37 @@
 import { useState } from 'react'
 
+const positionClasses = {
+  top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-2',
+  bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-2',
+  left: 'right-full top-1/2 transform -translate-y-1/2 mr-2',
+  right: 'left-full top-1/2 transform -translate-y-1/2 ml-2',
+}
+
 function Tooltip({ children, content, position = 'top' }) {
   const [isVisible, setIsVisible] = useState(false)
+  const offsetClass = positionClasses[position] ?? positionClasses.top
 
   return (
-    <div className="relative inline-block">
+    <div className="relative inline-flex">
       <div
+        className="cursor-pointer"
         onMouseEnter={() => setIsVisible(true)}
         onMouseLeave={() => setIsVisible(false)}
-        className="cursor-pointer bg-yellow-200 p-1 rounded"
-        title={content} // Fallback using native browser tooltip
+        onFocus={() => setIsVisible(true)}
+        onBlur={() => setIsVisible(false)}
+        title={content}
+        aria-label={content}
       >
         {children}
-        {isVisible && (
-          <div className="absolute z-50 px-3 py-2 text-sm text-white bg-red-500 rounded-lg shadow-lg whitespace-nowrap top-full left-0 mt-1">
-            {content}
-          </div>
-        )}
       </div>
+
+      {isVisible && (
+        <div
+          className={`absolute z-50 px-3 py-2 text-sm leading-snug text-white bg-black rounded-lg shadow-lg whitespace-nowrap pointer-events-none ${offsetClass}`}
+        >
+          {content}
+        </div>
+      )}
     </div>
   )
 }
